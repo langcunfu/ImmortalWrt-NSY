5034d10c3252fbbb00461928ee9a82f2d6a57413
langcunfu <55777476+langcunfu@users.noreply.github.com>
2026-08-27 19:34:44 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 保存OpenWrt源码根目录
+ROOT_DIR="$PWD"
 
 # 修改默认IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
@@ -109,4 +111,5 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-a
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+cd "${ROOT_DIR}"
 make defconfig
